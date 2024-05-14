0d17b012481d3a04201296206ab219e56e629571
Mariano <marianogotardo@gmail.com>
2024-05-14 13:24:36 -0300

    Ejercicio 1 hecho

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Setup
+Console.WriteLine("\n----- Setup -----\n");
 Console.WriteLine("Hello, World!");
 int a;
 int b;
@@ -6,3 +7,23 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+
+
+// Ejercicio 1
+Console.WriteLine("\n----- Ejercicio Numero 1 -----\n");
+float X, Y;
+bool checkParse;
+
+Console.Write("Ingrese un numero mayor a 0: ");
+checkParse = float.TryParse(Console.ReadLine(), out X);
+
+while ((checkParse == false) || (X <= 0))
+{
+    Console.WriteLine("\nERROR: Debe ingresar un NUMERO que sea MAYOR A '0'.");
+    Console.Write("Ingrese un numero: ");
+    checkParse = float.TryParse(Console.ReadLine(), out X);
+}
+
+
+Y = (1/X);
+Console.WriteLine("\nEl inverso del numero '" + X + "' es: '" + Y + "'");
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-**Trabajo Practico Numero 6**
-Introduccion a C# y .NET
+# Trabajo Practico Numero 6
+**Introduccion a C# y .NET**
+## Ejercicio 4: Preguntas y Respuestas
+1. ¿String es una tipo por valor o un tipo por referencia?
+
+2. ¿Qué secuencias de escape tiene el tipo string?
+
+3. ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
